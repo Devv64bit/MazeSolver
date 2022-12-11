2d15317f9cea3b41bc590c4394538c9d1fd98700
CBussom <cameron.bussom@gmail.com>
2022-12-10 21:25:12 -0500

    Added Save and Load Feature

--- MazeSolver/main.py
+++ MazeSolver/main.py
@@ -10,6 +10,7 @@ red = (255, 0, 0)
 blue = (0, 128, 255)
 path = (220, 253, 113)  # PATH
 
+
 WINDOW_HEIGHT = 800
 WINDOW_WIDTH = 720
 WIDTH = 50
@@ -28,21 +29,21 @@ START_POS, GOAL_POS = None, None
 
 pygame.display.set_caption("Maze Runners")
 
+def load_Grid():
+    with open("MazeSolver\patterns\default.txt", "r") as file:
+        grid = file.read()
+    START_POS, GOAL_POS = None, None
+    grid = [list(row) for row in grid.split("\n")]
 
-with open(r"C:\Users\bbuss\OneDrive\Documents\GitHub\MazeSolver\MazeSolver\patterns\default.txt", "r") as file:
-    grid = file.read()
-START_POS, GOAL_POS = None, None
-grid = [list(row) for row in grid.split("\n")]
-
-# Converting grid to required format
-for r in range(12):
-    for c in range(13):
-        if grid[r][c] == "S":
-            START_POS = r, c
-        if grid[r][c] == "G":
-            GOAL_POS = r, c
-        grid[r][c] = grid[r][c].replace(" ", "EMPTY").replace("G", "GOAL").replace("#", "WALL").replace("S", "START")
-
+    # Converting grid to required format
+    for r in range(12):
+        for c in range(13):
+            if grid[r][c] == "S":
+                START_POS = r, c
+            if grid[r][c] == "G":
+                GOAL_POS = r, c
+            grid[r][c] = grid[r][c].replace(" ", "EMPTY").replace("G", "GOAL").replace("#", "WALL").replace("S", "START")
+    return(grid)
 
 def remove_path(grid):
     for r in range(12):
@@ -50,11 +51,53 @@ def remove_path(grid):
             if grid[r][c] == "PATH":
                 grid[r][c] = "EMPTY"
 
+def clear_Grid(grid):
+    for r in range(12):
+        for c in range(13):
+            grid[r][c] = "EMPTY"
+
+def save_Grid(grid):
+    saveGrid = grid
+    remove_path(saveGrid)
+    for r in range(12):
+        for c in range(13):
+            if saveGrid[r][c] == "S":
+                START_POS = r, c
+            if saveGrid[r][c] == "G":
+                GOAL_POS = r, c
+            saveGrid[r][c] = saveGrid[r][c].replace("EMPTY", " ").replace("GOAL", "G").replace("WALL", "#").replace("START", "S")
+    file = open("MazeSolver\patterns\save.txt", "w")
+    for r in range(12):
+        file.write("\n")
+        for c in range(13):
+            file.write(saveGrid[r][c])
+
+
+
+def load_Saved_Grid():
+    with open("MazeSolver\patterns\save.txt", "r") as file:
+        loadGrid = file.read()
+        START_POS, GOAL_POS = None, None
+        loadGrid = [list(row) for row in loadGrid.split("\n")]
+
+
+    # Converting grid to required format
+    for r in range(12):
+        for c in range(13):
+            if loadGrid[r][c] == "S":
+                START_POS = r, c
+            if loadGrid[r][c] == "G":
+                GOAL_POS = r, c
+            loadGrid[r][c] = loadGrid[r][c].replace(" ", "EMPTY").replace("G", "GOAL").replace("#", "WALL").replace("S", "START")
+    return(loadGrid)
+
 
 def main():
-    global screen, CLOCK
-
+    grid = load_Grid()
+    print(grid)
+    global screen
     globIndex = -1
+
     pygame.init()
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     screen.fill(black)
@@ -138,16 +181,21 @@ def main():
                         globIndex = -1
 
                 if event.key == pygame.K_c:
-                    print("cleared the array")
-                    for r in range(12):
-                        for c in range(13):
-                            grid[r][c] = "EMPTY"
+                    print("cleared the Grid")
+                    clear_Grid(grid)
                 if event.key == pygame.K_RETURN:
                     remove_path(grid)
                     #path = DFS(grid, START_POS, GOAL_POS)
                     path = A_star(grid, START_POS, GOAL_POS)
                     for pos in path:
                         grid[pos[0]][pos[1]] = "PATH"
+                
+                if event.key == pygame.K_s:
+                    print("saved grid")
+                
+                if event.key == pygame.K_l:
+                    print("loaded grid")
+                    grid = load_Saved_Grid()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # User clicks the mouse. Get the position
@@ -185,6 +233,7 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
         for row in range(12):
             for column in range(13):
                 if grid[row][column] == "EMPTY":
@@ -200,33 +249,33 @@ def main():
                 elif grid[row][column] == "PATH":
                     color = blue
 
-                pygame.draw.rect(screen, color, [
-                                 (MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN, WIDTH, HEIGHT])
+                pygame.draw.rect(screen, color, [(MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row + MARGIN, WIDTH, HEIGHT])
+
         font = pygame.font.Font('freesansbold.ttf', 22)
         draw = font.render('Draw Mode:', True, white, black)
         controls = font.render('Controls:', True, white, black)
         pressQ = font.render('Press Q to change draw mode', True, white, black)
         clear = font.render('Press C to clear Maze', True, white, black)
+        pressEnter = font.render('Press Enter to Solve Maze', True, white, black)
+
         contRect = controls.get_rect()
         pressQRect = pressQ.get_rect()
         clearRect = clear.get_rect()
+        pressEnterRect = pressEnter.get_rect()
         drawRect = draw.get_rect()
 
-        contRect.center = ((WINDOW_HEIGHT // 2) + 100,
-                           (WINDOW_WIDTH // 2) + 320)
-        pressQRect.center = ((WINDOW_HEIGHT // 2) + 100,
-                             (WINDOW_WIDTH // 2) + 350)
-        clearRect.center = ((WINDOW_HEIGHT // 2) + 100,
-                            (WINDOW_WIDTH // 2) + 380)
-        drawRect.center = ((WINDOW_HEIGHT // 2) - 300,
-                           (WINDOW_WIDTH // 2) + 320)
+        contRect.center = ((WINDOW_HEIGHT // 2) + 100,(WINDOW_WIDTH // 2) + 320)
+        pressQRect.center = ((WINDOW_HEIGHT // 2) + 100,(WINDOW_WIDTH // 2) + 350)
+        clearRect.center = ((WINDOW_HEIGHT // 2) + 100,(WINDOW_WIDTH // 2) + 380)
+        pressEnterRect.center = ((WINDOW_HEIGHT // 2) + 100,(WINDOW_WIDTH // 2) + 410)
+        drawRect.center = ((WINDOW_HEIGHT // 2) - 300,(WINDOW_WIDTH // 2) + 320)
 
         screen.blit(draw, drawRect)
         screen.blit(controls, contRect)
         screen.blit(pressQ, pressQRect)
         screen.blit(clear, clearRect)
+        screen.blit(pressEnter, pressEnterRect)
 
         pygame.display.flip()
-
-
+load_Grid()
 main()
